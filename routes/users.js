9667dc8a9958fea295bc543aee641c6f81c02adf
lucasaugsue <lucasaugsue7@gmail.com>
2023-11-21 11:08:33 -0300

    criando o CRUD de users

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,63 @@
+var md5 = require('md5');
 var express = require('express');
 var router = express.Router();
 
 const CyclicDb = require("@cyclic.sh/dynamodb")
 const db = CyclicDb("weak-tuna-attireCyclicDB")
 
-const animals = db.collection("animals")
+const users = db.collection("users")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/list', async function(req, res, next) {
+  let list = await users.list(99)
+  let tmp = []
+
+  list = await (list.results || []).reduce(async (old, curr) =>  {
+    let item = await users.get(curr.key)
+    tmp.push(item)
+
+    old = tmp
+    return old 
+  }, [])
+
+  res.json(list)
 });
 
-router.get('/teste', async function(req, res, next) {
-  // res.send(`apenas testando uma nova rota!`);
+router.get('/by/:key', async function(req, res, next) {
+  let key = req.params.key
+  let item = await users.get(key)
 
-  // create an item in collection with key "leo"
-  let leo = await animals.set("leo", {
-    type: "cat",
-    color: "orange"
-  })
+  res.json(item)
+})
+
+router.post('/create', async function(req, res, next) {
+  let params = {...req.body}
+  let key = md5(params.nome)
+
+  await users.set(key, params)
+  let item = await users.get(key)
+
+  // console.log("item", item)
   
-  // get an item at key "leo" from collection animals
-  let item = await animals.get("leo")
-  console.log("item", {...item})
-
-  res.send(`resposta: ${item}`)
+  res.send('Criado com sucesso!')
 });
+
+router.patch('/edit/:key', async function(req, res, next) {
+  let params = {...req.body}
+  let key = req.params.key
+
+  await users.set(key, params)
+  let item = await users.get(key)
+
+  // console.log("item", item)
+
+  res.send('Editado com sucesso!')
+});
+
+router.delete('/delete/:key', async function(req, res, next) {
+  let key = req.params.key
+  await users.delete(key)
+
+  res.send('Deletado com sucesso!')
+}); 
 
 module.exports = router;
